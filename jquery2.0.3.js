5beb708e2b3dc56f650086f3f36c6c1f5404345e
liuwei9413 <570366990@qq.com>
2015-08-24 10:11:40 +0800

    jq study

--- jquery2.0.3.js
+++ jquery2.0.3.js
@@ -11,20 +11,6 @@
  *
  * Date: 2013-07-03T13:30Z
  */
-
-/*框架结构分析
-(21, 94) 		定义了一些变量和函数 jQuery = function(){};
-(96, 283) 		给JQ对象添加一些属性和方法
-(285, 347)		extend: jq继承方法
-(349, 817)		jQuery.extend: 扩展一些工具方法(底层方法)
-(877, 2856)		Sizzle : 复杂选择器的实现
-(2880, 3042)	callbacks : 回调函数，函数的统一管理
-(3043, 3183)	Deferred : 延迟对象 ：对异步的管理
-
-
-(8826)	 提供对外接口 window.jQuery = window.$ = $;
-*/
-
 (function( window, undefined ) {
 
 // Can't do this because several apps including ASP.NET trace
@@ -8841,3 +8827,30 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
 }
 
 })( window );
+
+
+
+/*
+框架结构分析
+(function(){
+	(21 , 94) 定义了一些变量和函数 jQuery = function(){};
+	(96 , 283) 给JQ对象，添加一些方法和属性
+	(285 , 347) extend : JQ的继承方法
+	(349 , 870) jQuery.extend() : 扩展一些工具方法
+	(877 , 2856)  Sizzle : 复杂选择器的实现
+	(2880 , 3042) Callbacks : 回调对象 : 对函数的统一管理
+	(3043 , 3183) Deferred : 延迟对象 : 对异步的统一管理
+	(3184 , 3295) support : 功能检测
+	(3308 , 3652) data() : 数据缓存
+	(3653 , 3797) queue() : 队列方法 : 执行顺序的管理
+	(3803 , 4299) attr() prop() val() addClass()等 : 对元素属性的操作
+	(4300 , 5128) on() trigger() : 事件操作的相关方法
+	(5140 , 6057) DOM操作 : 添加 删除 获取 包装 DOM筛选
+	(6058 , 6620) css() : 样式的操作
+	(6621 , 7854) 提交的数据和ajax() : ajax() load() getJSON()
+	(7855 , 8584) animate() : 运动的方法
+	(8585 , 8792) offset() : 位置和尺寸的方法
+	(8804 , 8821) JQ支持模块化的模式
+	(8826)  window.jQuery = window.$ = jQuery;
+})();
+*/
